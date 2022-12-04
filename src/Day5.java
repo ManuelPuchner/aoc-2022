27edce73483c8468cf50d4e8e85ab592637e1063
ManuelPuchner <75803474+ManuelPuchner@users.noreply.github.com>
2022-12-04 10:43:00 +0100

    feat: day5 - preps

---
+++ src/Day5.java
@@ -0,0 +1,11 @@
+public class Day5 extends Day {
+    @Override
+    public Object solvePart1() {
+        return null;
+    }
+
+    @Override
+    public Object solvePart2() {
+        return null;
+    }
+}
